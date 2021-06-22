436fbc345f25509094d271449c444fb2a0dab29a
AjitsinghKamal <ajitskamal@gmail.com>
2021-06-22 12:43:43 +0530

    feat: add service worker to make app work offline

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { render } from 'react-dom';
 import Router from './router/Router';
+import { registerSW } from 'virtual:pwa-register';
 
 import './styles/reset.css';
 import './styles/tokens.css';
@@ -12,3 +13,5 @@ render(
 	</StrictMode>,
 	document.querySelector('#root')
 );
+
+registerSW();
--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,11 @@ import path from 'path';
 import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
 import svgr from 'vite-plugin-svgr';
+import { VitePWA } from 'vite-plugin-pwa';
+
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [reactRefresh(), svgr()],
+	plugins: [reactRefresh(), VitePWA(), svgr()],
 	esbuild: {
 		jsxInject: `import React from "react"`,
 	},
